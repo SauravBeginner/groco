3930d6d7f06ab04329b92d6aea324af0b8303821
SauravBeginner <kundusaurav1996@gmail.com>
2024-06-04 05:19:38 +0530

    landing ui bug fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
@@ -13,8 +10,6 @@ import { Blogs } from "./components/Blogs";
 import { Footer } from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
