2928730de3ae70f9452ecf65c0215b0792fb30b4
SauravBeginner <kundusaurav1996@gmail.com>
2024-06-20 19:38:28 +0530

    env fixed

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://groco-nu.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://groco-nu.vercel.app",
+      "http://localhost:5174",
+    ],
     credentials: true,
   })
 );
