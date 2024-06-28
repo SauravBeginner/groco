fdbd220c9dc7489104e1137fc5a74aa7061465d2
SauravBeginner <kundusaurav1996@gmail.com>
2024-06-28 06:29:47 +0530

    server url fixed

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const baseURL: string = import.meta.env.VITE_BASE_URL;
-export const authURL: string = import.meta.env.VITE_BASE_URL + "/auth";
+export const baseURL: string =
+  import.meta.env.VITE_BASE_URL || "http://localhost:5000/api/v1";
+export const authURL: string = baseURL + "/auth";
 
 export const publicAxios = axios.create({
   baseURL,
