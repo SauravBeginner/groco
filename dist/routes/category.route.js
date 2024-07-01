7e3f25a047f3fd5a5116c78d2e51c881c71e008c
Saurav Kundu <93449443+SauravBeginner@users.noreply.github.com>
2024-07-02 03:26:52 +0530

    Delete dist directory
    deler

--- dist/routes/category.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const category_controller_1 = require("../controllers/category.controller");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.post("/create", authMiddleware_1.authenticateJWT, authMiddleware_1.authAdmin, category_controller_1.addCategory);
-exports.default = router;
--- dist/utils/jwt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client_URL = exports.base_URL = void 0;
-exports.base_URL = process.env.PROD_SERVER_URL || "http://localhost:5000/api/v1";
-exports.client_URL = process.env.CLIENT_URL || "http://localhost:5173";
