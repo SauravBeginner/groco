fd8d06a7994dfcd6e59c958fb36a2d28d68dfde3
SauravBeginner <kundusaurav1996@gmail.com>
2024-07-02 03:21:16 +0530

    env changed2

--- dist/middleware/authMiddleware.js
+++ dist/middleware/authMiddleware.js
@@ -7,10 +7,10 @@ exports.authAdmin = exports.authenticateJWT = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
 const authenticateJWT = (req, res, next) => {
-    var _a;
+    var _a, _b;
     try {
         const authHeader = (_a = req.headers) === null || _a === void 0 ? void 0 : _a.authorization;
-        const token = req.cookies.token || (authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(" ")[1]);
+        const token = ((_b = req === null || req === void 0 ? void 0 : req.cookies) === null || _b === void 0 ? void 0 : _b.token) || (authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(" ")[1]);
         if (!token) {
             return res.status(403).json({ error: "Unauthorized!" });
         }
